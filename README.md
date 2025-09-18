b7963b22ffe1114cb37f82da69e9b08ff650fb34
queenofthefairies <siobhan.physics@gmail.com>
2025-09-18 14:01:18 +1000

    finished updating plotting script

--- README.md
+++ README.md
@@ -1,5 +1,12 @@
 # Python4LSC code
-This contains code used to analyse data from liquid scintillation coincidence counting experiments in radionuclide metrology. It also includes a test dataset (Ho166m). The original version of the code was written in 2018; the code was updated in 2025 to account for accidental coincidences, and updates to various python packages. The 2025 scripts have a modular structure consisting of a `user` script and then `pandas4LSC_data_processing` to deal with raw data taken at different threshold voltages, plus `pandas4LSC_data_analysis` to do the regression on the data. Scripts to plot the results are still completely separate.
+This contains code used to analyse data from liquid scintillation coincidence counting experiments in radionuclide metrology. It also includes a test dataset (Ho166m). The original version of the code was written in 2018; the code was updated in 2025 to account for accidental coincidences, and updates to various python packages. The 2025 scripts have a modular structure consisting of:     
+ - `pandas4LSC_user` script, which calls functions in    
+    -  `pandas4LSC_data_processing` to deal with raw data taken at different threshold voltages,     
+    - plus `pandas4LSC_data_analysis` to do the regression on the data. 
+
+Scripts to plot the results are still completely separate.    
+- - `PlotStuff4LSC_user` script, which calls functions in    
+    -  `PlotStuff4LSC` to plot the linearised threshold data, along with the cubic or linear fit, and a separate residuals plot
 
 
 
--- python4LSC_code/PlotStuff4LSC.py
+++ python4LSC_code/PlotStuff4LSC.py
@@ -21,162 +21,211 @@ def f3(x, a, c, d):
     """ The cubic function y= a*x^3 + c*x + d """
     return a*x**3  + c*x + d
 
+def tick_function(X, what_to_plot):
+    if what_to_plot == 'B':
+        V = (1-X)*100
+    elif what_to_plot == 'BGC':
+        V = 1/(1+X)*100
+    return ["%.1f" % z for z in V]
+
 def plotter(reg_data_filename, fits_data_filename, what_to_plot, 
-            type_of_fit, type_of_reg, stand_dev):
+            type_of_fit, type_of_reg, x_max, fsize, pwidth, pheight):
+
+    # get summary about source, stand dev etc
+    summary_df = pd.read_excel(fits_data_filename, 
+                               sheet_name='Summary')
+    source_name_df = summary_df.loc[(summary_df['Function'] == 'Source name')]
+    source_name = source_name_df['Regression data'][9]
+    std_dev_df = summary_df.loc[(summary_df['Function'] == 'Standard deviation used as weights')]
+    stand_dev = std_dev_df['Regression data'][18]
 
     # read in regression data
-    regdf = pd.read_excel(reg_data_filename, index_col=0)
+    reg_df = pd.read_excel(reg_data_filename, index_col=0)
 
     if what_to_plot == 'BGC':
-        xdata = np.array(regdf['G/C-1 WM'])
-        ydata = np.array(regdf['BG/C WM'])
+        x_data = np.array(reg_df['G/C-1 WM'])
+        y_data = np.array(reg_df['BG/C WM'])
         if stand_dev == 'OSD':
-            xunc = np.array(regdf['Stdev of Mean G/C-1 (Obs)'])
-            yunc = np.array(regdf['Stdev of Mean BGC (Obs)'])
+            x_unc = np.array(reg_df['Stdev of Mean G/C-1 (Obs)'])
+            y_unc = np.array(reg_df['Stdev of Mean BGC (Obs)'])
         elif stand_dev == 'TSD':
-            xunc = np.array(regdf['Stdev of Mean G/C-1 (Theor)'])
-            yunc = np.array(regdf['Stdev of Mean BGC (Theor)'])
+            x_unc = np.array(reg_df['Stdev of Mean G/C-1 (Theor)'])
+            y_unc = np.array(reg_df['Stdev of Mean BGC (Theor)'])
 
     elif what_to_plot == 'B':
-        xdata = np.array(reg_df['1-C/G WM'])
-        ydata = np.array(reg_df['B WM'])
+        x_data = np.array(reg_df['1-C/G WM'])
+        y_data = np.array(reg_df['B WM'])
         if stand_dev == 'OSD':
-            xunc = np.array(regdf['Stdev of Mean 1-C/G (Obs)'])
-            yunc = np.array(regdf['Stdev of Mean B (Obs)'])
+            x_unc = np.array(reg_df['Stdev of Mean 1-C/G (Obs)'])
+            y_unc = np.array(reg_df['Stdev of Mean B (Obs)'])
             if type_of_fit == 'ODR':
-                yunc = np.array(regdf['Stdev of Mean B (Obs)(ODR only)'])
+                y_unc = np.array(reg_df['Stdev of Mean B (Obs)(ODR only)'])
         elif stand_dev == 'TSD':
-            xunc = np.array(regdf['Stdev of Mean 1-C/G (Theor)'])
-            yunc = np.array(regdf['Stdev of Mean B (Theor)'])
-
+            x_unc = np.array(reg_df['Stdev of Mean 1-C/G (Theor)'])
+            y_unc = np.array(reg_df['Stdev of Mean B (Theor)'])
+    print('read in regression data from {0}'.format(reg_data_filename))
+    
     # read in fits data
     sheet_dictionary = {'linear' : 'LinearFits', 'cubic': 'CubicFits'}
-    fits_df = pd.read_excel(fits_data_filename, index_col=None, usecols=range(1,9), 
+    cols_range_dictionary = {'linear' : range(1,9), 'cubic': range(1,11)}
+    fits_df = pd.read_excel(fits_data_filename, index_col=None, usecols=cols_range_dictionary[type_of_fit], 
                             sheet_name=sheet_dictionary[type_of_fit])
-    fitted_data_dictionary = {'BGC':'BG/C vs GC-1', 'B': 'B vs 1-C/G'}
+    fitted_data_dictionary = {'BGC':'BG/C vs G/C-1', 'B': 'B vs 1-C/G'}
     fit_method_dictionary = {'LS':'Least Squares', 'ODR':'Orthogonal Distance'}
     specific_fit_df = fits_df.loc[(fits_df['Regression data'] == fitted_data_dictionary[what_to_plot]) & (fits_df['Regression method'] == fit_method_dictionary[type_of_reg])]
-    
-    print('specific fit df')
+    print('read in fit information from {0}'.format(fits_data_filename))
+    print()
+    print('Plotting this specific {0} fit'.format(type_of_fit))
     print(specific_fit_df)
+    print()
     
     fit_params = []
     if type_of_fit == 'linear':
-        fit_gradient = specific_fit_df['gradient'][0]
-        fit_intercept = specific_fit_df['intercept'][0]
+        fit_gradient = specific_fit_df['gradient'].to_numpy()[0]
+        fit_intercept = specific_fit_df['intercept'].to_numpy()[0]
         fit_params = [fit_gradient, fit_intercept]
     elif type_of_fit == 'cubic':
-        fit_a = specific_fit_df['a'][0]
-        fit_c = specific_fit_df['c'][0]
-        fit_intercept = specific_fit_df['intercept'][0]
+        fit_a = specific_fit_df['a'].to_numpy()[0]
+        fit_c = specific_fit_df['c'].to_numpy()[0]
+        fit_intercept = specific_fit_df['intercept'].to_numpy()[0]
         fit_params = [fit_a,fit_c,fit_intercept]
-    activity_conc = specific_fit_df['Activity conc'][0]
-    activity_conc_rel_unc = specific_fit_df['rel unc intercept %'][0]
+    activity_conc = specific_fit_df['Activity conc'].to_numpy()[0]
+    activity_conc_rel_unc = specific_fit_df['rel unc intercept %'].to_numpy()[0]
+    activity_conc_abs_unc = activity_conc*activity_conc_rel_unc/100
 
+    fit_x_to_plot = np.linspace(0, x_max, 200)
+    if type_of_fit == 'linear':
+        fit_y_to_plot = f1(fit_x_to_plot, *fit_params)
+        residuals_to_plot = y_data - f1(x_data, *fit_params)
+    elif type_of_fit == 'cubic':
+        fit_y_to_plot = f3(fit_x_to_plot, *fit_params)
+        residuals_to_plot = y_data - f3(x_data, *fit_params)
+    
+    if what_to_plot == 'B':
+        x_label = r"$1 - N_C /N_\gamma$"
+        y_label = r"$N_\beta $ (s$^{-1}$ mg $^{-1}$)"
+        leg_pos = 3 # legend position
+        txt_pos = 1 # corner text position
+        plot_fig_filename = '{0}_B_vs_ineff_{1}_{2}_PLOT_{2}.png'.format(source_name, type_of_fit, type_of_reg, stand_dev)
+        resid_plot_fig_filename = '{0}_B_vs_ineff_{1}_{2}_RESIDUALS_PLOT_{3}.png'.format(source_name,type_of_fit,type_of_reg,stand_dev)
+
+    elif what_to_plot == 'BGC':
+        x_label = r"$N_\gamma / N_C - 1$"
+        y_label = r"$N_\beta N_\gamma / N_C $ (s$^{-1}$ mg $^{-1}$)"
+        leg_pos = 4 # legend position
+        txt_pos = 2 # corner text position
+        plot_fig_filename = '{0}_BGC_vs_GoC-1_{1}_{2}_PLOT_{3}.png'.format(source_name,type_of_fit,type_of_reg,stand_dev)
+        resid_plot_fig_filename = '{0}_BGC_vs_GoC-1_{1}_{2}_RESIDUALS_PLOT_{3}.png'.format(source_name,type_of_fit,type_of_reg,stand_dev)
+    
+    cornertext="Activity from {0} intercept: \n".format(type_of_fit) +r"{0:.4f} $\pm$ {1:.4f} MBq/g ({2:.4f}%)".format(activity_conc,
+                                                                                                                       activity_conc_abs_unc,
+                                                                                                                       activity_conc_rel_unc)
+    # MAKE PLOT   
+    f, ax = plt.subplots(1,1)
+    # plot regression data
+    ax.errorbar(x_data, y_data, xerr=x_unc, yerr=y_unc, fmt= 'o', capsize=2, label='{0}'.format(source_name))
+    # plot fit
+    ax.plot(fit_x_to_plot, fit_y_to_plot, label='{0} fit {1}'.format(type_of_fit,type_of_reg))
+    # tweak plot layout
+    plt.locator_params(axis='y', nbins=6)
+    plt.locator_params(axis='x', nbins=6)
+    plt.xticks(fontsize=fsize-1, rotation=0)
+    plt.yticks(fontsize=fsize-1, rotation=0)
+    plt.xlim(0,x_max)
+    ax.legend(loc=leg_pos, fontsize=fsize-1)
+    # add corner text
+    ax.add_artist(AnchoredText(cornertext,prop=dict(size=fsize-1),loc=txt_pos,frameon=False,borderpad=1))
+    # add axes labels
+    ax.set_xlabel(x_label,fontsize=fsize)
+    ax.set_ylabel(y_label,fontsize=fsize)
+
+    # plot secondary x-axis to show beta efficiency
+    ax2 = ax.twiny()
+    new_tick_locations = np.linspace(0, x_max, 6)
+    ax2.set_xlim(ax.get_xlim())
+    ax2.set_xticks(new_tick_locations)
+    ax2.set_xticklabels(tick_function(new_tick_locations,what_to_plot))
+    ax2.set_xlabel(r"$\beta$ efficiency %",fontsize=fsize)
+    plt.locator_params(axis='y', nbins=6)
+    plt.locator_params(axis='x', nbins=6)
+    plt.xticks(fontsize=fsize-1, rotation=0)
+    plt.yticks(fontsize=fsize, rotation=0)
+    plt.xlim(0,x_max)
+
+    # final plot
+    plt.tight_layout()
+    plt.gcf().set_size_inches(pwidth,pheight)
+    plt.savefig(plot_fig_filename,dpi=240)
+    plt.show()
+
+    print('done plotting data. plot saved to {0}'.format(plot_fig_filename))
+    print()
+
+    # MAKE RESIDUAL PLOT
+    f, ax = plt.subplots(1,1)
+    # plot zero line
+    ax.axhline(y=0,linewidth=1,color='C1')
+    # plot residuals
+    ax.errorbar(x_data, residuals_to_plot, xerr=x_unc, yerr=y_unc, fmt= 'o', capsize=2, label='{0}'.format(source_name))
+    # tweak plot layout
+    plt.locator_params(axis='y', nbins=6)
+    plt.locator_params(axis='x', nbins=6)
+    plt.xticks(fontsize=fsize-1, rotation=0)
+    plt.yticks(fontsize=fsize-1, rotation=0)
+    plt.xlim(0,x_max)
+    ax.legend(loc=leg_pos, fontsize=fsize-1)
+    # add axes labels
+    ax.set_xlabel(x_label,fontsize=fsize)
+    ax.set_ylabel('Residuals ' + y_label,fontsize=fsize)
+    # add plot title
+    ax.set_title('{0} {1} {2} residuals'.format(source_name, type_of_fit, type_of_reg))
+
+    # final plot
+    plt.tight_layout()
+    plt.gcf().set_size_inches(pwidth,pheight)
+    plt.savefig(resid_plot_fig_filename,dpi=240)
+    plt.show()
+
+
+    # f, (ax1,ax3) = plt.subplots(2, sharex=True, sharey=True)
+    # ax1.errorbar(x_dataB, residLT, xerr=0, yerr=y_uncB, fmt= 'o', capsize=2, color='#ff7f0e', label='Linear fit residuals')
+    # ax1.axhline(y=0,color='#1f77b4',linewidth=1)
+    # ax1.legend(loc='lower right', fontsize=10)
+    # ax1.set_title('{0} LS residuals'.format(sourcename))
+    # ax1.set_ylabel(r"$N_\beta $ (s$^{-1}$ mg $^{-1}$)")
+    # ax3.errorbar(x_dataB, residCT, xerr=0, yerr=y_uncB, fmt= 'o', capsize=2, color='#2ca02c',label='Cubic fit residuals')
+    # ax3.axhline(y=0,color='#1f77b4',linewidth=1)
+    # ax3.legend(loc='lower right',fontsize=10)
+    # ax3.set_xlabel(r"$1 - N_C / N_\gamma $")
+    # ax3.set_ylabel(r"$N_\beta $ (s$^{-1}$ mg $^{-1}$)")
+    # f.subplots_adjust(hspace=0)
+    # plt.xlim(xMinRB,xMaxRB)
+    # plt.ylim(yMinRB,yMaxRB)
+    # plt.tight_layout()
+    # plt.setp([a.get_xticklabels() for a in f.axes[:-1]], visible=False)
+    # plt.gcf().set_size_inches(6,4.5)
+    # plt.savefig('{0}Resid_B_vs_ineff_LeastSq{1}{2}.png'.format(sourcename,DorT,SD),dpi=240)
+    # plt.show()
+
+    # f, (ax1,ax3) = plt.subplots(2, sharex=True, sharey=True)
+    # ax1.errorbar(x_dataB, residLP, xerr=0, yerr=percy_unc, fmt= 'o', capsize=2, color='#ff7f0e', label='Linear fit residuals')
+    # ax1.axhline(y=0,color='#1f77b4',linewidth=1)
+    # ax1.legend(loc='lower right', fontsize=10)
+    # ax1.set_title('{0} LS residuals'.format(sourcename))
+    # ax1.set_ylabel("%")
+    # ax3.errorbar(x_dataB, residCP, xerr=0, yerr=percy_unc, fmt= 'o', capsize=2, color='#2ca02c',label='Cubic fit residuals')
+    # ax3.axhline(y=0,color='#1f77b4',linewidth=1)
+    # ax3.legend(loc='lower right',fontsize=10)
+    # ax3.set_xlabel(r"$1 - N_C / N_\gamma $")
+    # ax3.set_ylabel("%")
+    # f.subplots_adjust(hspace=0)
+    # plt.xlim(xMinRB,xMaxRB)
+    # plt.tight_layout()
+    # plt.setp([a.get_xticklabels() for a in f.axes[:-1]], visible=False)
+    # plt.gcf().set_size_inches(6,4.5)
+    # plt.savefig('{0}ResidP_B_vs_ineff_LeastSq{1}{2}.png'.format(sourcename,DorT,SD),dpi=240)
+    # plt.show()
     
 
     return
 
-# # LINEAR B vs 1-C/G
-# poptLW, pcovLW = curve_fit(f1, xdataB, ydataB, p0=None, sigma=yuncB, absolute_sigma=True)
-# xtp = np.linspace(0, xMaxB, 200)
-# ylinfitW = f1(xtp, *poptLW)
-
-# # CUBIC B vs 1-C/G
-# poptcW, pcovcW = curve_fit(f3, xdataB, ydataB, p0=None, sigma=yuncB, absolute_sigma=True)
-# ycfitW = f3(xtp, *poptcW)
-
-# #==============================================================================
-# # B vs 1-C/G PLOT MAKER
-# #==============================================================================
-# residLT = ydataB - f1(xdataB, *poptLW)
-# residCT = ydataB - f3(xdataB, *poptcW)
-# residLP = (ydataB - f1(xdataB, *poptLW))/ydataB*100
-# residCP = (ydataB - f3(xdataB, *poptcW))/ydataB*100
-# percyunc = yuncB / ydataB *100
-# unc_intL=np.sqrt(pcovLW[1][1])
-# unc_intC=np.sqrt(pcovcW[2][2])
-# reluncL=unc_intL/poptLW[1]*100
-# reluncC=unc_intC/poptcW[2]*100
-# actfromL=poptLW[1]/(branchingratio*1000)
-# actfromC=poptcW[2]/(branchingratio*1000)
-# uncL=unc_intL/poptLW[1]*actfromL
-# uncC=unc_intC/poptcW[2]*actfromC
-# cornertext="Activity from linear intercept: \n"+r"{0:.3f} $\pm$ {1:.3f} MBq/g ({2:.1f}%)".format(actfromL,uncL,reluncL)+"\n"+"Activity from cubic intercept: \n"+r"{0:.3f}  $\pm$ {1:.3f} MBq/g ({2:.1f}%)".format(actfromC,uncC,reluncC)
-
-# #PLOT   
-# f, ax = plt.subplots(1,1)
-# ax.errorbar(xdataB, ydataB, xerr=0, yerr=yuncB, fmt= 'o', capsize=2, label='{0}'.format(sourcename))
-# ax.plot(xtp, ylinfitW, label='Linear fit LS')
-# ax.plot(xtp, ycfitW, label='Cubic fit LS')
-# plt.locator_params(axis='y', nbins=6)
-# plt.locator_params(axis='x', nbins=6)
-# plt.xticks(fontsize=fsize-1, rotation=0)
-# plt.yticks(fontsize=fsize-1, rotation=0)
-# plt.xlim(0,xMaxB)
-# plt.ylim(yMinB,yMaxB)
-# ax.legend(loc=B_legpos, fontsize=fsize-1)
-# ax.add_artist(AnchoredText(cornertext,prop=dict(size=fsize-1),loc=B_txtpos,frameon=False,borderpad=1))
-# ax.set_xlabel(r"$1 - N_C /N_\gamma$",fontsize=fsize)
-# ax.set_ylabel(r"$N_\beta $ (s$^{-1}$ mg $^{-1}$)",fontsize=fsize)
-
-# ax2 = ax.twiny()
-# new_tick_locations = np.linspace(0, xMaxB, 6)
-# def tick_function(X):
-#     V = (1-X)*100
-#     return ["%.1f" % z for z in V]
-
-# ax2.set_xlim(ax.get_xlim())
-# ax2.set_xticks(new_tick_locations)
-# ax2.set_xticklabels(tick_function(new_tick_locations))
-# ax2.set_xlabel(r"$\beta$ efficiency %",fontsize=fsize)
-# plt.locator_params(axis='y', nbins=6)
-# plt.locator_params(axis='x', nbins=6)
-# plt.xticks(fontsize=fsize-1, rotation=0)
-# plt.yticks(fontsize=fsize, rotation=0)
-# plt.xlim(0,xMaxB)
-# plt.ylim(yMinB,yMaxB)
-# plt.tight_layout()
-# plt.gcf().set_size_inches(pwidth,pheight)
-# plt.savefig('{0}_B_vs_ineff_LeastSq_PLOT_{1}{2}.png'.format(sourcename,DorT,SD),dpi=240)
-# plt.show()
-
-# #RESIDUAL PLOTS
-# f, (ax1,ax3) = plt.subplots(2, sharex=True, sharey=True)
-# ax1.errorbar(xdataB, residLT, xerr=0, yerr=yuncB, fmt= 'o', capsize=2, color='#ff7f0e', label='Linear fit residuals')
-# ax1.axhline(y=0,color='#1f77b4',linewidth=1)
-# ax1.legend(loc='lower right', fontsize=10)
-# ax1.set_title('{0} LS residuals'.format(sourcename))
-# ax1.set_ylabel(r"$N_\beta $ (s$^{-1}$ mg $^{-1}$)")
-# ax3.errorbar(xdataB, residCT, xerr=0, yerr=yuncB, fmt= 'o', capsize=2, color='#2ca02c',label='Cubic fit residuals')
-# ax3.axhline(y=0,color='#1f77b4',linewidth=1)
-# ax3.legend(loc='lower right',fontsize=10)
-# ax3.set_xlabel(r"$1 - N_C / N_\gamma $")
-# ax3.set_ylabel(r"$N_\beta $ (s$^{-1}$ mg $^{-1}$)")
-# f.subplots_adjust(hspace=0)
-# plt.xlim(xMinRB,xMaxRB)
-# plt.ylim(yMinRB,yMaxRB)
-# plt.tight_layout()
-# plt.setp([a.get_xticklabels() for a in f.axes[:-1]], visible=False)
-# plt.gcf().set_size_inches(6,4.5)
-# plt.savefig('{0}Resid_B_vs_ineff_LeastSq{1}{2}.png'.format(sourcename,DorT,SD),dpi=240)
-# plt.show()
-
-# f, (ax1,ax3) = plt.subplots(2, sharex=True, sharey=True)
-# ax1.errorbar(xdataB, residLP, xerr=0, yerr=percyunc, fmt= 'o', capsize=2, color='#ff7f0e', label='Linear fit residuals')
-# ax1.axhline(y=0,color='#1f77b4',linewidth=1)
-# ax1.legend(loc='lower right', fontsize=10)
-# ax1.set_title('{0} LS residuals'.format(sourcename))
-# ax1.set_ylabel("%")
-# ax3.errorbar(xdataB, residCP, xerr=0, yerr=percyunc, fmt= 'o', capsize=2, color='#2ca02c',label='Cubic fit residuals')
-# ax3.axhline(y=0,color='#1f77b4',linewidth=1)
-# ax3.legend(loc='lower right',fontsize=10)
-# ax3.set_xlabel(r"$1 - N_C / N_\gamma $")
-# ax3.set_ylabel("%")
-# f.subplots_adjust(hspace=0)
-# plt.xlim(xMinRB,xMaxRB)
-# plt.tight_layout()
-# plt.setp([a.get_xticklabels() for a in f.axes[:-1]], visible=False)
-# plt.gcf().set_size_inches(6,4.5)
-# plt.savefig('{0}ResidP_B_vs_ineff_LeastSq{1}{2}.png'.format(sourcename,DorT,SD),dpi=240)
-# plt.show()
